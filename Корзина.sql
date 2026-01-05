51af05507ddb8c33c1a51fb825a3fafeea0394ff
Roman Kozlov <romanc0zlov@yandex.ru>
2026-01-05 13:04:33 +0300

    Add files via upload

---
+++ Корзина.sql
@@ -0,0 +1,145 @@
+CREATE TABLE "users" (
+  "id" integer PRIMARY KEY,
+  "first_name" varchar,
+  "last_name" varchar,
+  "email" varchar,
+  "phone_number" varchar,
+  "created_at" timestamp,
+  "tg_username" varchar
+);
+
+CREATE TABLE "item" (
+  "id" integer PRIMARY KEY,
+  "name" varchar,
+  "base_price" decimal,
+  "rating" smallint,
+  "reviews_quantity" decimal
+);
+
+CREATE TABLE "category" (
+  "id" integer PRIMARY KEY,
+  "name" varchar
+);
+
+CREATE TABLE "favorites" (
+  "user_id" integer,
+  "item_id" integer,
+  PRIMARY KEY ("user_id", "item_id")
+);
+
+CREATE TABLE "cart" (
+  "id" integer PRIMARY KEY,
+  "user_id" integer
+);
+
+CREATE TABLE "cart_item" (
+  "id" integer PRIMARY KEY,
+  "cart_id" integer,
+  "item_id" integer,
+  "quantity" decimal,
+  "price" decimal,
+  "user_id" integer
+);
+
+CREATE TABLE "option" (
+  "id" integer PRIMARY KEY,
+  "item_id" integer,
+  "name" varchar
+);
+
+CREATE TABLE "opt_value" (
+  "id" integer PRIMARY KEY,
+  "option_id" integer,
+  "value" varchar,
+  "price_delta" decimal
+);
+
+CREATE TABLE "seller" (
+  "id" integer PRIMARY KEY,
+  "name" varchar,
+  "rating" smallint
+);
+
+CREATE TABLE "seller_rating" (
+  "id" smalliant PRIMARY KEY,
+  "value" decimal
+);
+
+CREATE TABLE "item_rating" (
+  "id" smallint PRIMARY KEY,
+  "value" decimal
+);
+
+CREATE TABLE "cart_item_opt_value" (
+  "cart_item_id" integer,
+  "opt_value_id" integer
+);
+
+CREATE TABLE "item_category" (
+  "item_id" integer,
+  "category_id" integer
+);
+
+CREATE TABLE "seller_item" (
+  "id" integer PRIMARY KEY,
+  "item_id" integer,
+  "seller_id" integer,
+  "price" decimal,
+  "available" boolean
+);
+
+CREATE TABLE "orders" (
+  "id" integer PRIMARY KEY,
+  "user_id" integer,
+  "item_id" integer,
+  "seller_id" integer,
+  "price" decimal,
+  "created_at" timestamp
+);
+
+CREATE TABLE "users_orders" (
+  "user_id" integer,
+  "itam_id" integer
+);
+
+COMMENT ON COLUMN "users"."tg_username" IS 'логин в тг, к которому привязан акк';
+
+ALTER TABLE "favorites" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "users" ADD FOREIGN KEY ("id") REFERENCES "cart" ("user_id");
+
+ALTER TABLE "opt_value" ADD FOREIGN KEY ("option_id") REFERENCES "option" ("id");
+
+ALTER TABLE "option" ADD FOREIGN KEY ("item_id") REFERENCES "item" ("id");
+
+ALTER TABLE "cart_item" ADD FOREIGN KEY ("cart_id") REFERENCES "cart" ("id");
+
+ALTER TABLE "item" ADD FOREIGN KEY ("id") REFERENCES "cart_item" ("item_id");
+
+ALTER TABLE "seller" ADD FOREIGN KEY ("rating") REFERENCES "seller_rating" ("id");
+
+ALTER TABLE "item" ADD FOREIGN KEY ("rating") REFERENCES "item_rating" ("id");
+
+ALTER TABLE "cart_item_opt_value" ADD FOREIGN KEY ("cart_item_id") REFERENCES "cart_item" ("id");
+
+ALTER TABLE "cart_item_opt_value" ADD FOREIGN KEY ("opt_value_id") REFERENCES "opt_value" ("id");
+
+ALTER TABLE "item_category" ADD FOREIGN KEY ("category_id") REFERENCES "category" ("id");
+
+ALTER TABLE "item_category" ADD FOREIGN KEY ("item_id") REFERENCES "item" ("id");
+
+ALTER TABLE "favorites" ADD FOREIGN KEY ("item_id") REFERENCES "item" ("id");
+
+ALTER TABLE "seller_item" ADD FOREIGN KEY ("seller_id") REFERENCES "seller" ("id");
+
+ALTER TABLE "seller_item" ADD FOREIGN KEY ("item_id") REFERENCES "item" ("id");
+
+ALTER TABLE "users" ADD FOREIGN KEY ("id") REFERENCES "cart_item" ("user_id");
+
+ALTER TABLE "users_orders" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "users_orders" ADD FOREIGN KEY ("user_id") REFERENCES "orders" ("id");
+
+ALTER TABLE "users_orders" ADD FOREIGN KEY ("itam_id") REFERENCES "item" ("id");
+
+ALTER TABLE "users_orders" ADD FOREIGN KEY ("itam_id") REFERENCES "orders" ("item_id");
